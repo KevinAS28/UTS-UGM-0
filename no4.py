ae657256c79953f038df46f54c150942790531fd
kevin <kevinagusto28@gmail.com>
2020-10-22 08:57:53 +0700

    problem description in no4.py

--- no4.py
+++ no4.py
@@ -1,3 +1,25 @@
+"""
+DESKRIPSI SOAL
+Keindahan puisi dinilai dari seberapa "mirip" akhiran suatu baris dengan baris yang lain yang berdekatan. Diinputkan dua buah string yang merupakan dua baris puisi. Beri nilai keindahan dengan cara menghitung berapa banyak huruf yang mirip diakhir kedua string tersebut.
+
+PETUNJUK MASUKAN
+Dua buah string dengan panjang huruf yang sama, tidak mengandung spasi dan hanya terdiri dari huruf tidak kapital
+
+PETUNJUK KELUARAN
+Outputkan sebuah bilangan bulat sesuai penjelasan di atas
+
+CONTOH MASUKAN 1
+adindaku
+ocintaku
+CONTOH KELUARAN 1
+3
+CONTOH MASUKAN 2
+engkauseinda
+cahyapurnama
+CONTOH KELUARAN 2
+1
+"""
+
 puisi = [input()[::-1] for i in range(2)]
 lenght = list(map(len, puisi))
 

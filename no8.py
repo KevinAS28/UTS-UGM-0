231691c421e8a0bf70019bb7bc91ad8082c01f79
kevin <kevinagusto28@gmail.com>
2020-10-22 09:45:35 +0700

    no 8

---
+++ no8.py
@@ -0,0 +1,21 @@
+"""
+DESKRIPSI SOAL
+Buatlah program yang membaca sebuah bilangan A (0< A <=5) dan program akan menampilkan sebuah bilangan B (nilai B adalah 0-4), sehingga A+B habis dibagi 5
+
+PETUNJUK MASUKAN
+Input terdiri atas 1 angka
+
+PETUNJUK KELUARAN
+Outputkan angka B
+
+CONTOH MASUKAN
+1
+CONTOH KELUARAN
+4
+CONTOH MASUKAN
+3
+CONTOH KELUARAN
+2
+"""
+
+print(5-int(input()))
